34337d47ac24d3761c13f1dee61912acbd52702d
zeezlelove <zeezlelove@gmail.com>
2019-05-12 19:55:46 +0900

    파이썬 팁입니다.

---
+++ 리스트 비어있는지 확인법.py
@@ -0,0 +1,5 @@
+n = []
+if bool(n) == False:
+    print("리스트에 값이 없습니다.")
+else:
+    print("리스트에 값이 있습니다.")
---
+++ 프로그램 돌아가는 시간 체크법.py
@@ -0,0 +1,6 @@
+import timeit
+start = timeit.default_timer()
+# 코드
+# ...
+end = timeit.default_timer()
+print(end-start)
